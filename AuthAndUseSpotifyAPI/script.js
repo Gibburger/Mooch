442e9f3cde465cbfda0e1cc2efcd5fa77537db6b
ElliotGibbons <epgibbons@mavs.coloradomesa.edu>
2024-03-06 22:51:24 -0700

    pushit

--- AuthAndUseSpotifyAPI/script.js
+++ AuthAndUseSpotifyAPI/script.js
@@ -2,7 +2,87 @@ var redirect_uri = "http://localhost:5500/AuthAndUseSpotifyAPI/frontPage.html";
 var client_id = "";
 var client_secret = "";
 
-const AUTHORIZE = "https://accounts.spotify.com/authorize"
+const AUTHORIZE = "https://accounts.spotify.com/authorize";
+const TOKEN = "https://accounts.spotify.com/api/token";
+
+function onPageLoad() //determians if user has been rederected or not on startup
+{
+    //everytime page loads must retrieve the client id and client secret just cuz
+    client_id = localStorage.getItem("client_id");
+    client_secret = localStorage.getItem("client_secret")
+
+    if(window.location.search.length > 0) // if we did get the autho code already
+    {
+        handleRedirect();
+    }
+}
+
+function handleRedirect()
+{
+    let code = getCode();
+    fetchAccessToken(code);
+    window.history.pushState("","", redirect_uri); //remove param from url so you have a clean url everytime page refreshes
+}
+
+function fetchAccessToken(code) //creates another url to spotify to get an access token
+{
+    //url needs the code recieved through user permission, client id, and client secret
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code;
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    callAuthorizationApi(body);
+}
+
+function callAuthorizationApi(body) //idk what this does
+{
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+function handleAuthorizationResponse()
+{
+    if(this.status == 200) //checks if succsess
+    {
+        var data = JSON.parse(this.responseText); 
+        console.log(data); //for debug purpouses
+        var data = JSON.parse(this.responseText);
+        if(data.access_token != undefined)
+        {
+            access_token = data.access_token; //if we got an access token
+            localStorage.setItem("access_token", access_token); //if so save it in local storage
+        }
+        if(data.refresh_token != undefined)
+        {
+            refresh_token = data.refresh_token; //if got refresh token 
+            localStorage.setItem("refresh_token", refresh_token); //if so save refresh token in local storage
+        }
+        onPageLoad();
+    }
+    else //if not display the error
+    {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+
+function getCode() //gets the code seperated from rest of the url 
+{
+    let code = null;
+    const queryString = window.location.search;
+    if(queryString.length > 0) //checks if there is data inputed 
+    {
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get('code');
+    }
+    return code;
+}
 
 function requestAuthorization()
 {
@@ -11,14 +91,14 @@ function requestAuthorization()
     localStorage.setItem("client_id", client_id);
     localStorage.setItem("client_secret", client_secret); //in real app you should not expose your client_secret to the user
     
-    
+
     //constructing request author link
     let url = AUTHORIZE; // client id
     url += "?client_id=" + client_id; 
     url += "&response_type=code"; 
     url += "&redirect_uri=" + encodeURI(redirect_uri) //redirect uri
     url += "&show_dialog=true"; //optional, will always display spotify dialog, so does not save the authication for dev purposes, set to false for final release
-    url += "&scope=user-read-private-user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private"
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private user-top-read";
     //^scope of permisions that we are asking for, can be longer or shorter depending on what permissions are needed
     /*
     url is asking for the following permissions
@@ -30,6 +110,37 @@ function requestAuthorization()
         5.read playback state
         6.read recently played
         7.read private
-    */
+    */  
     window.location.href = url; //show spotify's authorization screen
+}
+
+// Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
+async function fetchWebApi(endpoint, method, body) {
+  const token = access_token;
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+    body:JSON.stringify(body)
+  });
+  return await res.json();
+}
+
+async function getTopTracks(){
+  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+  return (await fetchWebApi(
+    'v1/me/top/tracks?time_range=long_term&limit=5', 'GET'
+  )).items;
+}
+
+async function CallTopTracks()
+{
+    const topTracks = await getTopTracks();
+    console.log(
+        topTracks?.map(
+            ({name, artists}) =>
+            `${name} by ${artists.map(artist => artist.name).join(', ')}`
+        )
+    );
 }
